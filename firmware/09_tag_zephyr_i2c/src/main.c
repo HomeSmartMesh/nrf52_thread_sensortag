fbae8db71295a5a2d12732a9c0f4bb5d38147459
wass <wassim.filali@gmail.com>
2021-04-17 12:10:16 +0200

    pinging all i2c sensors

--- firmware/09_tag_zephyr_i2c/src/main.c
+++ firmware/09_tag_zephyr_i2c/src/main.c
@@ -5,13 +5,13 @@
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
-#define SLEEP_TIME_MS   1000
-
 void main(void)
 {
-	LOG_INF("Hello from Sensor Tag");
+	LOG_INF("Hello from Sensor Tag I2C example");
 
 	const struct device *i2c_dev;
+	int ret;
+
 	i2c_dev = device_get_binding("I2C_0");
 	if (!i2c_dev) {
 		LOG_INF("I2C: Device driver not found.\n");
@@ -19,30 +19,27 @@ void main(void)
 	}
 
 	uint8_t data[16];
-	int ret;
 	uint16_t addr = 0x48;
-	ret = i2c_read(i2c_dev, &data[0], 1, addr);
+	ret = i2c_reg_read_byte(i2c_dev, addr, 0, &data[0]);
 	if (ret) {
-		LOG_INF("I2C: read failed %X.", addr);
+		LOG_ERR("I2C: i2c_reg_read_byte() @%X failed %X.",addr, ret);
 	} else {
-		LOG_INF("I2C: success %X.", addr);
+		LOG_INF("VEML6030 @%X reg[0] 'ALS_CONF' = %d",addr,data[0]);
 	}
 
 	addr = 0x40;
-	ret = i2c_read(i2c_dev, &data[0], 1, addr);
+	ret = i2c_reg_read_byte(i2c_dev, addr, 0xE7, &data[0]);
 	if (ret) {
-		LOG_INF("I2C: read failed %X.", addr);
+		LOG_ERR("I2C: i2c_reg_read_byte() @%X failed %X.",addr, ret);
 	} else {
-		LOG_INF("I2C: success %X.", addr);
+		LOG_INF("MS8607 @%X 'Relative Humidity RH' reg[0xE7] 'user register' = %d",addr,data[0]);
 	}
 
 	addr = 0x76;
-	ret = i2c_read(i2c_dev, &data[0], 1, addr);
+	ret = i2c_reg_read_byte(i2c_dev, addr, 0, &data[0]);
 	if (ret) {
-		LOG_INF("I2C: read failed %X.", addr);
+		LOG_ERR("I2C: i2c_reg_read_byte() @%X failed %X.",addr, ret);
 	} else {
-		LOG_INF("I2C: success %X.", addr);
+		LOG_INF("MS8607 @%X 'Pressure and Temperature' reg[0] 'ADC Read' => %d",addr,data[0]);
 	}
-
-
 }
